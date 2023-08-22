ee151ec53815bc49223960966d7ccf4284fb6602
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-08-22 18:57:13 -0300

    Add files via upload

---
+++ EX8POO/Comparador.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX8POO
+{
+    internal class Comparador
+    {
+        private double valor1;
+        private double valor2;
+
+        public Comparador(double v1, double v2)
+        {
+            valor1 = v1;
+            valor2 = v2;
+        }
+
+        public void CompararEExibirMaior()
+        {
+            if (valor1 > valor2)
+            {
+                Console.WriteLine("O primeiro valor é maior: " + valor1);
+            }
+            else if (valor2 > valor1)
+            {
+                Console.WriteLine("O segundo valor é maior: " + valor2);
+            }
+            else
+            {
+                Console.WriteLine("Os valores são iguais.");
+            }
+        }
+    }
+}
---
+++ EX8POO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX8POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro valor: ");
+            double valor1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            double valor2 = Convert.ToDouble(Console.ReadLine());
+
+            Comparador comparador = new Comparador(valor1, valor2);
+            comparador.CompararEExibirMaior();
+        }
+    }
+}
